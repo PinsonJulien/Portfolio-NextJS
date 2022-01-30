21af62d6481b3661c181c44b4b300edba8fb7234
Julien Pinson <pinsonj01@gmail.com>
2022-01-30 12:50:04 +0100

    Création d'un composant burger button

---
+++ components/buttons/burger/burger.tsx
@@ -0,0 +1,32 @@
+import styles from './burger.module.scss';
+
+export function BurgerButton(
+  {get, set, className, childClassName} : 
+  {
+    get: any;
+    set: any;
+    className?: string;
+    childClassName?: string;
+  }) {
+  return (
+    <button 
+      onClick={() => set(!get)}  
+      className={`
+        group
+        space-y-0.5
+        items-center
+        px-3 
+        py-2 
+        border 
+        rounded
+        border-teal-400 
+        hover:border-white
+        ${className}
+      `}
+    >
+      <div className={`${childClassName} h-0.5 w-3 bg-teal-200 group-hover:bg-white ${!get ? styles.fadeIn : styles.fadeOut}`}></div>
+      <div className={`${childClassName} h-0.5 w-3 bg-teal-200 group-hover:bg-white`}></div>
+      <div className={`${childClassName} h-0.5 w-3 bg-teal-200 group-hover:bg-white ${!get ? styles.fadeIn : styles.fadeOut}`}></div>
+    </button>
+  )
+}
